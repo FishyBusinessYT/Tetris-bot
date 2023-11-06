b3935b072246d02e73c6adf27e113298a6fcbcf2
FishyBusinessYT <cristobalj.gonzalez@gmail.com>
2023-11-06 14:51:58 -0300

    .

--- Train.py
+++ Train.py
@@ -14,26 +14,28 @@ def offspring(path1, path2, n):
         for x in range(4):
             offspring.write(p[r(0, 1)][x])
 
-#Instance generation
-for i in range(8):
-    with open("./gen0/org" + str(i) + ".txt", "r+") as f:
-        org = Organism(f.readline(), f.readline(), f.readline(), f.readline())
-        org.play(0)
-        scores[i] = int(input("Enter organism score: "))
+while True:
+    for i in range(8):
+        with open("./gen0/org" + str(i) + ".txt", "r+") as f:
+            org = Organism(f.readline(), f.readline(), f.readline(), f.readline())
+            org.play(0)
+            scores[i] = int(input("Enter organism score: "))
 
-elite1 = scores.index(sorted(scores)[-1])
-elite2 = scores.index(sorted(scores)[-2])
+    elite1 = scores.index(sorted(scores)[-1])
+    elite2 = scores.index(sorted(scores)[-2])
 
-copy_file("./gen0/org" + str(elite1) + ".txt", "./gen1/org0.txt")
-copy_file("./gen0/org" + str(elite2) + ".txt", "./gen1/org1.txt")
-offspring("./gen0/org" + str(elite1) + ".txt", "./gen0/org" + str(elite2) + ".txt", 2)
-offspring("./gen0/org" + str(elite1) + ".txt", "./gen0/org" + str(elite2) + ".txt", 3)
+    copy_file("./gen0/org" + str(elite1) + ".txt", "./gen1/org0.txt")
+    copy_file("./gen0/org" + str(elite2) + ".txt", "./gen1/org1.txt")
+    offspring("./gen0/org" + str(elite1) + ".txt", "./gen0/org" + str(elite2) + ".txt", 2)
+    offspring("./gen0/org" + str(elite1) + ".txt", "./gen0/org" + str(elite2) + ".txt", 3)
 
-for i in range(4, 8):
-    offspring(
-        "./gen0/org" + scores.index(sorted(scores)[r(0, 6)]) + ".txt",
-        "./gen0/org" + scores.index(sorted(scores)[r(0, 6)]) + ".txt",
-        i)
+    for i in range(4, 8):
+        offspring(
+            "./gen0/org" + scores.index(sorted(scores)[r(0, 6)]) + ".txt",
+            "./gen0/org" + scores.index(sorted(scores)[r(0, 6)]) + ".txt",
+            i)
 
-for i in range(8):
-    copy_file("./gen1/org" + str(i) + ".txt", "./gen0/org" + str(i) + ".txt")
+    for i in range(8):
+        copy_file("./gen1/org" + str(i) + ".txt", "./gen0/org" + str(i) + ".txt")
+
+    input("Pause for stopping the training. Press enter to continue.")
