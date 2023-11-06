665d82c95df6fc2a5968aba92e0cc4d276916ef1
FishyBusinessYT <cristobalj.gonzalez@gmail.com>
2023-11-06 14:49:45 -0300

    .

--- Train.py
+++ Train.py
@@ -8,9 +8,9 @@ def copy_file(path1, path2):
         with open(path2, "w") as f2:
             f2.write(f1.read())
 
-def offspring(path1, path2, i):
+def offspring(path1, path2, n):
     p = [open(path1, "r").readlines(), open(path2, "r").readlines()]
-    with open("./gen1/org" + str(i) + ".txt", "w") as offspring:
+    with open("./gen1/org" + str(n) + ".txt", "w") as offspring:
         for x in range(4):
             offspring.write(p[r(0, 1)][x])
 
@@ -28,3 +28,12 @@ copy_file("./gen0/org" + str(elite1) + ".txt", "./gen1/org0.txt")
 copy_file("./gen0/org" + str(elite2) + ".txt", "./gen1/org1.txt")
 offspring("./gen0/org" + str(elite1) + ".txt", "./gen0/org" + str(elite2) + ".txt", 2)
 offspring("./gen0/org" + str(elite1) + ".txt", "./gen0/org" + str(elite2) + ".txt", 3)
+
+for i in range(4, 8):
+    offspring(
+        "./gen0/org" + scores.index(sorted(scores)[r(0, 6)]) + ".txt",
+        "./gen0/org" + scores.index(sorted(scores)[r(0, 6)]) + ".txt",
+        i)
+
+for i in range(8):
+    copy_file("./gen1/org" + str(i) + ".txt", "./gen0/org" + str(i) + ".txt")
