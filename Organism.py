5251d70c27007f2450a1960cd95660ab22344e05
FishyBusinessYT <cristobalj.gonzalez@gmail.com>
2023-11-06 11:57:27 -0300

    .

--- Organism.py
+++ Organism.py
@@ -1,4 +1,13 @@
+from PIL.ImageGrab import grab
+from numpy import array
+from time import sleep
+from Interface import click
+from Interface import move
+from Cost import cost
+
 class Organism():
     def __init__(self, holesW:int, buriedW:int, heightW:int, terrainW:int) -> None:
         self.holesW = holesW
         self.buriedW = buriedW
+        self.heightW = heightW
+        self.terrainW = terrainW
